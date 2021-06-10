89211011d91dc088b772b0ee46aee02f0f81a09e
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2021-06-10 14:17:57 +0600

    refactor: added aliases for long keycodes

--- keyboards/moonlander/keymaps/liljaylj/keymap.c
+++ keyboards/moonlander/keymaps/liljaylj/keymap.c
@@ -35,24 +35,43 @@ enum {
     TD_PENT_PEQL,
 };
 
+#define SYSGRV              LT(SYSTEM,KC_GRV)
+#define MEHTAB              MT(MOD_MEH,KC_TAB)
+#define HYPRESC             MT(MOD_HYPR,KC_ESC)
+#define LCTLZ               LCTL_T(KC_Z)
+#define LCTLHOM             LCTL_T(KC_HOME)
+#define RCTLSLSH            RCTL_T(KC_SLSH)
+#define RCTLEND             RCTL_T(KC_END)
+#define RSFTENT             RSFT_T(KC_ENT)
+
+#define SYMSCLN             LT(SYMBOLS,KC_SCLN)
+#define MEDQUOT             LT(MEDIA,KC_QUOTE)
+#define SYSBSPC             LT(SYSTEM,KC_BSPACE)
+#define TTSYM               TT(SYMBOLS)
+#define TTMED               TT(MEDIA)
+
+#define TDENEQ              TD(TD_PENT_PEQL)
+
+#define WUPAIR              WEBUSB_PAIR
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_moonlander(
-        LT(SYSTEM,KC_GRV),      KC_1,           KC_2,       KC_3,           KC_4,       KC_5,   XXXXXXX,                                        XXXXXXX,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,               LT(3,KC_BSPACE),
-        MT(MOD_MEH,KC_TAB),     KC_Q,           KC_W,       KC_E,           KC_R,       KC_T,   XXXXXXX,                                        XXXXXXX,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLASH,
-        MT(MOD_HYPR,KC_ESC),    KC_A,           KC_S,       KC_D,           KC_F,       KC_G,   XXXXXXX,                                        XXXXXXX,    KC_H,       KC_J,       KC_K,       KC_L,       LT(1,KC_SCLN),      LT(2,KC_QUOTE),
-        KC_LSFT,                LCTL_T(KC_Z),   KC_X,       KC_C,           KC_V,       KC_B,                                                               KC_N,       KC_M,       KC_COMM,    KC_DOT,     RCTL_T(KC_SLSH),    RSFT_T(KC_ENT),
-        LCTL_T(KC_HOME),        KC_LGUI,        KC_LALT,    TT(SYMBOLS),    TT(MEDIA),                  TT(SYSTEM),             LCTL_T(KC_APP),             KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,            RCTL_T(KC_END),
-                                                                        KC_SPC,     KC_BSPC,    RGUI_T(KC_ENT),                 LGUI_T(KC_SPC), KC_TAB, KC_ENT
+        SYSGRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,   XXXXXXX,                                    XXXXXXX,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       SYSBSPC,
+        MEHTAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,   XXXXXXX,                                    XXXXXXX,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLASH,
+        HYPRESC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,   XXXXXXX,                                    XXXXXXX,    KC_H,       KC_J,       KC_K,       KC_L,       SYMSCLN,    MEDQUOT,
+        KC_LSFT,    LCTLZ,      KC_X,       KC_C,       KC_V,       KC_B,                                                           KC_N,       KC_M,       KC_COMM,    KC_DOT,     RCTLSLSH,   RSFTENT,
+        LCTLHOM,    KC_LGUI,    KC_LALT,    TTSYM,      TTMED,                          TT(SYSTEM),             LCTL_T(KC_APP),                 KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    RCTLEND,
+                                                                KC_SPC,     KC_BSPC,    RGUI_T(KC_ENT),         LGUI_T(KC_SPC), KC_TAB, KC_ENT
     ),
 
     [SYMBOLS] = LAYOUT_moonlander(
-        XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      XXXXXXX,                                XXXXXXX,    KC_F6,              KC_F7,  KC_F8,      KC_F9,      KC_F10,     KC_DEL,
-        _______,    KC_F11,     KC_F12,     KC_LCBR,    KC_RCBR,    KC_PGUP,    XXXXXXX,                                XXXXXXX,    KC_PAST,            KC_P7,  KC_P8,      KC_P9,      KC_SLCK,    KC_PPLS,
-        _______,    KC_MINS,    KC_EQL,     KC_LPRN,    KC_RPRN,    KC_PGDN,    XXXXXXX,                                XXXXXXX,    KC_PSLS,            KC_P4,  KC_P5,      KC_P6,      _______,    KC_PMNS,
-        _______,    KC_UNDS,    KC_PLUS,    KC_LBRC,    KC_RBRC,    KC_INS,                                                         TD(TD_PENT_PEQL),   KC_P1,  KC_P2,      KC_P3,      KC_NLCK,    _______,
-        _______,    _______,    _______,    _______,    KC_CAPS,                        _______,                _______,                                KC_P0,  KC_PDOT,    _______,    _______,    _______,
+        XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      XXXXXXX,                                XXXXXXX,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,
+        _______,    KC_F11,     KC_F12,     KC_LCBR,    KC_RCBR,    KC_PGUP,    XXXXXXX,                                XXXXXXX,    KC_PAST,    KC_P7,      KC_P8,      KC_P9,      KC_SLCK,    KC_PPLS,
+        _______,    KC_MINS,    KC_EQL,     KC_LPRN,    KC_RPRN,    KC_PGDN,    XXXXXXX,                                XXXXXXX,    KC_PSLS,    KC_P4,      KC_P5,      KC_P6,      _______,    KC_PMNS,
+        _______,    KC_UNDS,    KC_PLUS,    KC_LBRC,    KC_RBRC,    KC_INS,                                                         TDENEQ,     KC_P1,      KC_P2,      KC_P3,      KC_NLCK,    _______,
+        _______,    _______,    _______,    _______,    KC_CAPS,                        _______,                _______,                        KC_P0,      KC_PDOT,    _______,    _______,    _______,
                                                                 _______,    _______,    _______,                _______,    _______,    _______
     ),
 
@@ -66,11 +85,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [SYSTEM] = LAYOUT_moonlander(
-        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,                                  RESET,          XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
-        _______,    MU_TOG,     NK_ON,      NK_OFF,     RESET,      XXXXXXX,    EEP_RST,                                EEP_RST,        MU_TOG,     MU_MOD,     LED_LEVEL,  XXXXXXX,    KC_BRK,     KC_PWR,
-        _______,    AU_TOG,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    WEBUSB_PAIR,                            WEBUSB_PAIR,    AU_ON,      AU_OFF,     XXXXXXX,    XXXXXXX,    KC_WAKE,    KC_SLEP,
-        _______,    CK_TOGG,    XXXXXXX,    XXXXXXX,    VRSN,       XXXXXXX,                                                            NK_TOGG,    DM_REC1,    DM_REC2,    DM_RSTP,    XXXXXXX,    _______,
-        _______,    _______,    _______,    XXXXXXX,    XXXXXXX,                        _______,                RGB_TOG,                            DM_PLY1,    DM_PLY2,    _______,    _______,    _______,
+        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    RESET,                                  RESET,      XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
+        _______,    MU_TOG,     NK_ON,      NK_OFF,     RESET,      XXXXXXX,    EEP_RST,                                EEP_RST,    MU_TOG,     MU_MOD,     LED_LEVEL,  XXXXXXX,    KC_BRK,     KC_PWR,
+        _______,    AU_TOG,     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    WEBUSB_PAIR,                            WUPAIR,     AU_ON,      AU_OFF,     XXXXXXX,    XXXXXXX,    KC_WAKE,    KC_SLEP,
+        _______,    CK_TOGG,    XXXXXXX,    XXXXXXX,    VRSN,       XXXXXXX,                                                        NK_TOGG,    DM_REC1,    DM_REC2,    DM_RSTP,    XXXXXXX,    _______,
+        _______,    _______,    _______,    XXXXXXX,    XXXXXXX,                        _______,                RGB_TOG,                        DM_PLY1,    DM_PLY2,    _______,    _______,    _______,
                                                                 RGB_HUI,    RGB_HUD,    RGB_SAD,                RGB_SAI,    RGB_VAD,    RGB_VAI
     ),
 
--- keyboards/preonic/keymaps/liljaylj/keymap.c
+++ keyboards/preonic/keymaps/liljaylj/keymap.c
@@ -33,22 +33,39 @@ enum {
     TD_PENT_PEQL,
 };
 
+#define SYSGRV              LT(SYSTEM,KC_GRV)
+#define MEHTAB              MT(MOD_MEH,KC_TAB)
+#define HYPRESC             MT(MOD_HYPR,KC_ESC)
+#define LCTLZ               LCTL_T(KC_Z)
+#define LCTLHOM             LCTL_T(KC_HOME)
+#define RCTLSLSH            RCTL_T(KC_SLSH)
+#define RCTLEND             RCTL_T(KC_END)
+#define RSFTENT             RSFT_T(KC_ENT)
+
+#define SYMSCLN             LT(SYMBOLS,KC_SCLN)
+#define MEDQUOT             LT(MEDIA,KC_QUOTE)
+#define SYSBSPC             LT(SYSTEM,KC_BSPACE)
+#define TTSYM               TT(SYMBOLS)
+#define TTMED               TT(MEDIA)
+
+#define TDENEQ              TD(TD_PENT_PEQL)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_preonic_grid(
-        LT(SYSTEM,KC_GRV),      KC_1,           KC_2,       KC_3,           KC_4,       KC_5,   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,               LT(3,KC_BSPACE),
-        MT(MOD_MEH,KC_TAB),     KC_Q,           KC_W,       KC_E,           KC_R,       KC_T,   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLASH,
-        MT(MOD_HYPR,KC_ESC),    KC_A,           KC_S,       KC_D,           KC_F,       KC_G,   KC_H,       KC_J,       KC_K,       KC_L,       LT(1,KC_SCLN),      LT(2,KC_QUOTE),
-        KC_LSFT,                LCTL_T(KC_Z),   KC_X,       KC_C,           KC_V,       KC_B,   KC_N,       KC_M,       KC_COMM,    KC_DOT,     RCTL_T(KC_SLSH),    RSFT_T(KC_ENT),
-        LCTL_T(KC_HOME),        KC_LGUI,        KC_LALT,    TT(SYMBOLS),    TT(MEDIA),  KC_SPC, KC_SPC,     KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,            RCTL_T(KC_END)
+        SYSGRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       SYSBSPC,
+        MEHTAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLASH,
+        HYPRESC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       SYMSCLN,    MEDQUOT,
+        KC_LSFT,    LCTLZ,      KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     RCTLSLSH,   RSFTENT,
+        LCTLHOM,    KC_LGUI,    KC_LALT,    TTSYM,      TTMED,      KC_SPC,     KC_SPC,     KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    RCTLEND
     ),
 
     [SYMBOLS] = LAYOUT_preonic_grid(
-        XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,              KC_F7,  KC_F8,      KC_F9,      KC_F10,     KC_DEL,
-        _______,    KC_F11,     KC_F12,     KC_LCBR,    KC_RCBR,    KC_PGUP,    KC_PAST,            KC_P7,  KC_P8,      KC_P9,      KC_SLCK,    KC_PPLS,
-        _______,    KC_MINS,    KC_EQL,     KC_LPRN,    KC_RPRN,    KC_PGDN,    KC_PSLS,            KC_P4,  KC_P5,      KC_P6,      _______,    KC_PMNS,
-        _______,    KC_UNDS,    KC_PLUS,    KC_LBRC,    KC_RBRC,    KC_INS,     TD(TD_PENT_PEQL),   KC_P1,  KC_P2,      KC_P3,      KC_NLCK,    _______,
-        _______,    _______,    _______,    _______,    KC_CAPS,    _______,    _______,            KC_P0,  KC_PDOT,    _______,    _______,    _______
+        XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,
+        _______,    KC_F11,     KC_F12,     KC_LCBR,    KC_RCBR,    KC_PGUP,    KC_PAST,    KC_P7,      KC_P8,      KC_P9,      KC_SLCK,    KC_PPLS,
+        _______,    KC_MINS,    KC_EQL,     KC_LPRN,    KC_RPRN,    KC_PGDN,    KC_PSLS,    KC_P4,      KC_P5,      KC_P6,      _______,    KC_PMNS,
+        _______,    KC_UNDS,    KC_PLUS,    KC_LBRC,    KC_RBRC,    KC_INS,     TDENEQ,     KC_P1,      KC_P2,      KC_P3,      KC_NLCK,    _______,
+        _______,    _______,    _______,    _______,    KC_CAPS,    _______,    _______,    KC_P0,      KC_PDOT,    _______,    _______,    _______
     ),
 
     [MEDIA] = LAYOUT_preonic_grid(
