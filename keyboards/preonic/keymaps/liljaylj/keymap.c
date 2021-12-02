4446ed77baf0d49d2489650f19aa02a10acbf93f
Zhanibek Adilbekov <zhanibek.adilbekov@pm.me>
2021-12-03 01:29:49 +0600

    feat: use 1x2uC instead of grid

--- keyboards/preonic/keymaps/liljaylj/keymap.c
+++ keyboards/preonic/keymaps/liljaylj/keymap.c
@@ -53,44 +53,44 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [BASE] = LAYOUT_preonic_grid(
+    [BASE] = LAYOUT_preonic_1x2uC(
         SYSGRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       SYSBSPC,
         MEHTAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
         HYPRESC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       SYMSCLN,    MEDQUOT,
         KC_LSFT,    LCTLZ,      KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     RCTLSLSH,   RSFTENT,
-        LCTLHOM,    KC_LGUI,    KC_LALT,    TTSYM,      TTMED,      KC_SPC,     KC_SPC,     KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    RCTLEND
+        LCTLHOM,    KC_LGUI,    KC_LALT,    TTSYM,      TTMED,            KC_SPC,           KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    RCTLEND
     ),
 
-    [SYMBOLS] = LAYOUT_preonic_grid(
+    [SYMBOLS] = LAYOUT_preonic_1x2uC(
         XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,
         _______,    KC_F11,     KC_F12,     KC_LCBR,    KC_RCBR,    KC_PGUP,    KC_PAST,    KC_P7,      KC_P8,      KC_P9,      KC_SLCK,    KC_PPLS,
         _______,    KC_MINS,    KC_EQL,     KC_LPRN,    KC_RPRN,    KC_PGDN,    KC_PSLS,    KC_P4,      KC_P5,      KC_P6,      _______,    KC_PMNS,
         _______,    KC_UNDS,    KC_PLUS,    KC_LBRC,    KC_RBRC,    KC_INS,     TDENEQ,     KC_P1,      KC_P2,      KC_P3,      KC_NLCK,    _______,
-        KC_LCTL,    _______,    _______,    _______,    KC_CAPS,    _______,    _______,    KC_P0,      KC_PDOT,    _______,    _______,    KC_RCTL
+        KC_LCTL,    _______,    _______,    _______,    KC_CAPS,          _______,          KC_P0,      KC_PDOT,    _______,    _______,    KC_RCTL
     ),
 
-    [MEDIA] = LAYOUT_preonic_grid(
+    [MEDIA] = LAYOUT_preonic_1x2uC(
         XXXXXXX,    XXXXXXX,    KC_BTN4,    KC_BTN3,    KC_BTN5,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
         _______,    KC_F15,     KC_BTN2,    KC_MS_U,    KC_BTN1,    KC_WH_U,    KC_ACL0,    KC_ACL1,    KC_ACL2,    KC_MRWD,    KC_MFFD,    XXXXXXX,
         _______,    XXXXXXX,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_WH_D,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    KC_MPLY,    _______,
         _______,    KC_BRID,    KC_BRIU,    KC_F14,     KC_WH_L,    KC_WH_R,    KC_MSTP,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_PSCR,    _______,
-        _______,    _______,    _______,    KC_APP,     _______,    _______,    _______,    KC_MPRV,    KC_MNXT,    _______,    _______,    _______
+        _______,    _______,    _______,    KC_APP,     _______,          _______,          KC_MPRV,    KC_MNXT,    _______,    _______,    _______
     ),
 
-    [SYSTEM] = LAYOUT_preonic_grid(
+    [SYSTEM] = LAYOUT_preonic_1x2uC(
         _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,
         _______,    MU_TOG,     NK_ON,      NK_OFF,     RESET,      XXXXXXX,    MU_TOG,     MU_MOD,     XXXXXXX,    XXXXXXX,    KC_BRK,     KC_PWR,
         _______,    AU_TOG,     XXXXXXX,    XXXXXXX,    XXXXXXX,    TO(GAMING), AU_ON,      AU_OFF,     XXXXXXX,    XXXXXXX,    KC_WAKE,    KC_SLEP,
         _______,    CK_TOGG,    XXXXXXX,    XXXXXXX,    VRSN,       XXXXXXX,    NK_TOGG,    DM_REC1,    DM_REC2,    DM_RSTP,    XXXXXXX,    _______,
-        _______,    _______,    _______,    XXXXXXX,    XXXXXXX,    _______,    _______,    DM_PLY1,    DM_PLY2,    _______,    _______,    _______
+        _______,    _______,    _______,    XXXXXXX,    XXXXXXX,          _______,          DM_PLY1,    DM_PLY2,    _______,    _______,    _______
     ),
 
-    [GAMING] = LAYOUT_preonic_grid(
+    [GAMING] = LAYOUT_preonic_1x2uC(
         KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSLS,
         KC_ESC,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    RSFTENT,
-        KC_LCTL,    KC_LGUI,    KC_LALT,    TO(BASE),   XXXXXXX,    KC_SPC,     KC_SPC,     KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    KC_RCTL
+        KC_LCTL,    KC_LGUI,    KC_LALT,    TO(BASE),   XXXXXXX,          KC_SPC,           KC_LBRC,    KC_RBRC,    KC_RALT,    KC_RGUI,    KC_RCTL
     ),
 
 };
