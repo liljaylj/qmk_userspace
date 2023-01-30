ac0770f12b675330cc706b1b69e0c228b914a972
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2023-01-31 01:43:32 +0600

    feat(autoshift): make it optional

--- users/liljaylj/liljaylj.c
+++ users/liljaylj/liljaylj.c
@@ -6,10 +6,13 @@ void eeconfig_init_keymap(void) { }
 void eeconfig_init_user(void) {
     // reset EEPROM
     user_config.raw = 0;
-    user_config.auto_shift_enabled = false;
-    eeconfig_update_user(user_config.raw);
 
+#ifdef AUTO_SHIFT_ENABLE
+    user_config.auto_shift_enabled = false;
     autoshift_disable();
+#endif
+
+    eeconfig_update_user(user_config.raw);
 
     eeconfig_init_keymap();
 }
@@ -21,16 +24,20 @@ void keyboard_post_init_user(void) {
     // read EEPROM
     user_config.raw = eeconfig_read_user();
 
+#ifdef AUTO_SHIFT_ENABLE
+    autoshift_disable();
     if (user_config.auto_shift_enabled) {
         autoshift_enable();
     }
     else {
         autoshift_disable();
     }
+#endif
 
     keyboard_post_init_keymap();
 }
 
+#ifdef AUTO_SHIFT_ENABLE
 __attribute__ ((weak))
 bool get_custom_auto_shifted_key_keymap(uint16_t keycode, keyrecord_t *record) {
     return false;
@@ -43,6 +50,7 @@ bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
     }
     return get_custom_auto_shifted_key_keymap(keycode, record);
 }
+#endif
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     // 1 - KC_PENT, 2 - KC_PEQL
@@ -62,6 +70,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
             break;
+#ifdef AUTO_SHIFT_ENABLE
         case AS_ON:
         case AS_OFF:
             if (record->event.pressed) {
@@ -70,6 +79,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return true;
             }
             break;
+#endif
     }
     return process_record_keymap(keycode, record);
 }
