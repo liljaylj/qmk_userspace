0dc4c9e4a1c84fca8ee7201bfdd18ac064b2a347
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2025-10-23 05:20:20 +0500

    fix: error: a label can only be part of a statement and a declaration is not a statements

--- users/liljaylj/liljaylj.c
+++ users/liljaylj/liljaylj.c
@@ -78,20 +78,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
         case KC_BSPC:
-            static bool delkey_registered;
-            if (record->event.pressed) {
-                if (mod_state & MOD_BIT(KC_LSFT)) {
-                    del_mods(MOD_BIT(KC_LSFT));
-                    register_code(KC_DEL);
-                    delkey_registered = true;
-                    set_mods(mod_state);
-                    return false;
-                }
-            } else {
-                if (delkey_registered) {
-                    unregister_code(KC_DEL);
-                    delkey_registered = false;
-                    return false;
+            {
+                static bool delkey_registered;
+                if (record->event.pressed) {
+                    if (mod_state & MOD_BIT(KC_LSFT)) {
+                        del_mods(MOD_BIT(KC_LSFT));
+                        register_code(KC_DEL);
+                        delkey_registered = true;
+                        set_mods(mod_state);
+                        return false;
+                    }
+                } else {
+                    if (delkey_registered) {
+                        unregister_code(KC_DEL);
+                        delkey_registered = false;
+                        return false;
+                    }
                 }
             }
             break;
