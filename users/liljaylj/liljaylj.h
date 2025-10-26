4e0b29da728b2c2eb813412aeedce3908a56df2c
Zhanibek Adilbekov <zhanibek.adilbekov@proton.me>
2025-10-27 02:26:59 +0500

    feat: use space as layer lock for CUR layer

--- users/liljaylj/liljaylj.h
+++ users/liljaylj/liljaylj.h
@@ -53,7 +53,7 @@ user_config_t user_config;
 // middle button(s) function on SYM layer
 #define SYMMID              _______
 // middle button(s) function on CUR layer
-#define CURMID              KC_UNDS
+#define CURMID              QK_LLCK
 // middle button(s) function on SYS layer
 #define SYSMID              KC_MPLY
 
